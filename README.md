ebe9282437e3957ddb75f430ec47f0fbcc8c93e5
Kevin Fu <kevin.fu808@gmail.com>
2023-06-10 00:26:39 -0700

    Initial commit with pruning branches

---
+++ README.md
@@ -0,0 +1,1 @@
+# This script updates, rebases, prunes, and deletes duplicate Git branches in a local repository.
---
+++ prune-branches.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Ensure we're in a Git repository
+if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+  echo "This is not a Git repository."
+  exit 1
+fi
+
+# Fetch updates from the remote repository and prune any deleted remote branches
+git fetch origin --prune
+
+# List all local branches
+for branch in $(git branch --format "%(refname:short)"); do
+  # Skip if we're already on the 'main' branch
+  if [ "$branch" == "main" ]; then
+    continue
+  fi
+
+  # Checkout the branch
+  git checkout "$branch"
+
+  # Rebase the branch against origin/main
+  git rebase origin/main
+
+  # If there are conflicts, abort the rebase and print a message
+  if [ $? -ne 0 ]; then
+    echo "Rebase of $branch has conflicts. Aborting rebase."
+    git rebase --abort
+    continue
+  fi
+
+  # Check if there are any differences between the branch and main
+  if [ -z "$(git diff origin/main..HEAD)" ]; then
+    # If there are no differences, delete the branch
+    echo "No differences between $branch and main. Deleting $branch."
+    git checkout main
+    git branch -d "$branch"
+  fi
+done
+
+# Go back to the main branch when done
+git checkout main
+
